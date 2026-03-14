537a8b0c302ce99be22bcb0657719208eacef30e
Kanipakapatnam PreamKumar <preammanasa123@gmail.com>
2026-03-14 10:01:25 +0530

    Add files via upload

---
+++ Samplepatterns2.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Samplepatterns2
+{
+    public static void main(String[] args) 
+    {
+        Scanner ob=new Scanner(System.in);
+        System.out.println("enter the number of rows:");
+        int rows=ob.nextInt();
+        for (int i = 1; i <= rows; i++) 
+        {
+            for (int j = 1; j <= rows - i; j++) 
+            {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= (2 * i - 1); k++) 
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
+
